0782851949ca61167df9a7efcb67fd291d5cc486
MihailPy <mihailpy02@gmail.com>
2026-02-04 14:50:46 +0300

    Added a Citi class and created functions for loading and saving files. These finctions have been tested for functionaly in main.py .

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import json
+from src.city import City, load_from_json, save_to_json
+
+
 def main():
     print("Hello from sun-set!")
+    cities = load_from_json("cities.json")
+    if isinstance(cities, str):
+        print(f"{cities}")
+        cities = []
+
+    if isinstance(cities, list):  # type: ignore
+        if not cities:
+            city = City("Moscow", "Moscow", 55.7558, 37.6173, "Europe/Moscow", 156)
+            cities.append(city)
+            save_to_json(cities, "cities.json")
+    print(json.dumps([city.__dict__ for city in cities], indent=4, ensure_ascii=False))
 
 
 if __name__ == "__main__":
---
+++ src/city.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+import json
+
+
+@dataclass
+class City:
+    name: str
+    region: str
+    lat: float
+    lon: float
+    timezone: str
+    elevation: int
+
+
+def save_to_json(cities: list[City], filename: str):
+    with open(filename, "w") as f:
+        json.dump([vars(c) for c in cities], f)
+
+
+def load_from_json(filename: str) -> list[City] | str:
+    try:
+        with open(filename) as f:
+            return [City(**data) for data in json.load(f)]
+    except FileNotFoundError:
+        return "Ошибка: Файл не найден. Проверьте путь к файлу."
+
+    except json.JSONDecodeError:
+        return "Ошибка: Файл поврежден или имеет неверный формат JSON."
+
+    except PermissionError:
+        return "Ошибка: Нет прав доступа для чтения этого файла."
+
+    except Exception as e:
+        return f"Неизвестная ошибка: {e}"
