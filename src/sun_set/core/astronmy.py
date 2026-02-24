527071baf9d35f963736233ac5f71760958e120f
MihailPy <mihailpy02@gmail.com>
2026-02-24 17:54:25 +0300

    Improved the code. Improved the loop.

--- src/sun_set/core/astronmy.py
+++ src/sun_set/core/astronmy.py
@@ -10,30 +10,31 @@ from sun_set.models.sunset import MonthData, Source, SunsetEntry, YearData
 
 
 def get_city_sunset(city: City, year: int, weekday1: int, weekday2: int) -> YearData:
+    obs = Observer(city.lat, city.lon, city.elevation)
+    city_tz = ZoneInfo(city.timezone)
+    weekdays_to_check = {weekday1, weekday2}
 
     sunset_city_year = []
-    sunset_city_month = []
+
     for month in range(1, 13):
+        sunset_city_month = []
         _, last_day = calendar.monthrange(year, month)
 
         for day in range(1, last_day + 1):
-            data = datetime.date(year, month, day)
-            weekday = data.weekday()
+            current_date = datetime.date(year, month, day)
 
-            if weekday == weekday1 or weekday == weekday2:
-                # day_s = f"{day:02d}"
+            if current_date.weekday() in weekdays_to_check:
+                res = sunset(obs, date=current_date, tzinfo=city_tz)
 
-                obs = Observer(city.lat, city.lon, city.elevation)
-                city_tz = ZoneInfo(city.timezone)
-                res = sunset(obs, date=data, tzinfo=city_tz)
-                hour = int(res.time().hour)
-                minute = f"{int(res.time().minute):02d}"
+                time_str = res.strftime("%H:%M")
+                # hour = int(res.time().hour)
+                # minute = f"{int(res.time().minute):02d}"
                 res_day = SunsetEntry(
-                    day, weekday, f"{hour}:{minute}", Source.CALCULATED
+                    day, current_date.weekday(), time_str, Source.CALCULATED
                 )
                 sunset_city_month.append(res_day)
-        res_month = MonthData(month, sunset_city_month)
-        sunset_city_year.append(res_month)
-        sunset_city_month = []
+
+        if sunset_city_month:
+            sunset_city_year.append(MonthData(month, sunset_city_month))
 
     return YearData(year, Source.CALCULATED, sunset_city_year)
