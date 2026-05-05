032aff3f22d26fbda149ca7eb376d46751aa968b
MihailPy <mihailpy02@gmail.com>
2026-05-05 09:52:36 +0300

    refactor(models): delete unused code

--- src/sun_set/models/sunset.py
+++ src/sun_set/models/sunset.py
@@ -2,12 +2,6 @@ from dataclasses import dataclass
 from enum import Enum, auto
 
 
-class AutoName(Enum):
-    @staticmethod
-    def _generate_next_value_(name, start, count, last_values):
-        return name
-
-
 class Source(Enum):
     CALCULATED = auto()
     EDITED = auto()
