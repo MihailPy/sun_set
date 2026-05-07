182e0bd38494a0ffa94f56fa818f310c45dede3f
MihailPy <mihailpy02@gmail.com>
2026-05-07 18:03:17 +0300

    Add image export settings models

--- src/sun_set/image_export/settings.py
+++ src/sun_set/image_export/settings.py
@@ -1,1 +1,40 @@
 # dataclass-модели настроек
+
+
+from dataclasses import dataclass
+
+
+@dataclass
+class ImageSettings:
+    width: int
+    height: int
+    background_color: str
+    template_path: str | None
+
+
+@dataclass
+class TextSettings:
+    font_path: str | None
+    font_size: int
+    color: str
+
+
+@dataclass
+class MonthBlockSettings:
+    x: int
+    y: int
+
+
+@dataclass
+class LayoutSettings:
+    row_height: int
+    meeting_offset_x: int
+    sunset_offset_x: int
+    month_blocks: dict[int, MonthBlockSettings]
+
+
+@dataclass
+class ExportImageSettings:
+    image: ImageSettings
+    text: TextSettings
+    layout: LayoutSettings
