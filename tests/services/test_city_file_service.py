387b74db6d348c16db9443d83a3796dba189de9c
MihailPy <mihailpy02@gmail.com>
2026-06-02 14:59:50 +0300

    test: update city file service pathches

--- tests/services/test_city_file_service.py
+++ tests/services/test_city_file_service.py
@@ -6,20 +6,20 @@ from sun_set.services.city_file_service import (
 )
 
 
-@patch("sun_set.services.city_file_service.load_from_json")
-def test_load_cities_from_file(mock_load_from_json):
-    mock_load_from_json.return_value = ([], None)
+@patch("sun_set.services.city_file_service.load_cities_from_json")
+def test_load_cities_from_file(mock_load_cities_from_json):
+    mock_load_cities_from_json.return_value = ([], None)
 
     result = load_cities_from_file("cities.json")
 
     assert result == ([], None)
-    mock_load_from_json.assert_called_once_with("cities.json")
+    mock_load_cities_from_json.assert_called_once_with("cities.json")
 
 
-@patch("sun_set.services.city_file_service.save_to_json")
-def test_save_cities_to_file(mock_save_to_json):
+@patch("sun_set.services.city_file_service.save_cities_to_json")
+def test_save_cities_to_file(mock_save_cities_to_json):
     cities = []
 
     save_cities_to_file(cities, "cities.json")
 
-    mock_save_to_json.assert_called_once_with(cities, "cities.json")
+    mock_save_cities_to_json.assert_called_once_with(cities, "cities.json")
